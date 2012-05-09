ab2e1677ba3a3f671a0fb0069ac4cb1ca31e5861
Christian Koptein <christian@koptein.de>
2012-05-09 22:26:01 +0200

    Numbers will be ignored

--- rot13.py
+++ rot13.py
@@ -15,11 +15,13 @@ output_string = ''
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 for letter in input_string:
-    myindex  = alphabet.index(letter)
-    if myindex >= 13:
-        output_string = output_string+alphabet[myindex-13]    
-    
-    else:
-        output_string = output_string+alphabet[myindex+13]
+# Exclude Numbers:
+    if not letter.isdigit():
+        myindex  = alphabet.index(letter.lower())
+        if myindex >= 13:
+            output_string = output_string+alphabet[myindex-13]    
+        
+        else:
+            output_string = output_string+alphabet[myindex+13]
 
 print  output_string
