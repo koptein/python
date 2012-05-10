2b8ab53ad9a2d07bec139b0b67e1fbd137497651
Stephan Saathoff <saathoff@bytemine.net>
2012-05-10 20:33:28 +0200

    Enable upper-/lowercase "encryption".

--- rot13.py
+++ rot13.py
@@ -12,12 +12,18 @@ else :
 
 output_string = ''
 
+# This looks really dirty, there should be an other way.
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alphabet_upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+#print type(alphabet)
 
 for letter in input_string:
-
-    # Getting the type of an object.
     #print letter + " -> " + str(type(letter))
+
+    # I can't believe this works...
+    use = alphabet_upper
+    if letter.islower():
+        use = alphabet
 
     # Exclude Numbers:
     if letter.isdigit():
@@ -26,9 +32,9 @@ for letter in input_string:
     else:
         myindex  = alphabet.index(letter.lower())
         if myindex >= 13:
-            output_string = output_string+alphabet[myindex-13]    
+                output_string = output_string+use[myindex-13]
         
         else:
-            output_string = output_string+alphabet[myindex+13]
+            output_string = output_string+use[myindex+13]
 
 print  output_string
