6b3e8d695d59ca3101ad147c749b9896abd923e4
Christian Koptein <christian@koptein.de>
2012-05-12 19:24:31 +0200

    DEV: -f flag funktioniert, debug ausgaben hinzugefügt

--- rot13.py
+++ rot13.py
@@ -1,24 +1,50 @@
 #!/usr/bin/python
 
 from sys import argv
+import fileinput
 
 #Initializing variables
 input_string = ''
 output_string = ''
 alphabet = []
 alphabet_upper = []
+use_argv = False
 
+#functions
+
+def readfile( filename ):
+    text = ""
+    myfile = open(filename)
+    for line in myfile:
+        if line != "":
+            text =  text + line
+    print "DEBUG:filecontent_= "+text
+    return text
+    
 # Ask for text if more or less than one argument.
-if len(argv) != 2:
+if len(argv) < 2:
     input_string = raw_input('Enter Text: ')
-else :
+else:
+# check for filehandling
+
+    for element in argv:
+        if element == "-f":
+            print "DEBUG: File argument detected"
+            filename = argv[argv.index(element)+1]
+            print "DEBUG:filename= "+filename 
+            input_string = readfile(filename)
+
+if use_argv: 
     input_string = argv[1]
+#Filling the Alphabet arrays from the ascii table
 
 for char in map(chr, xrange(97, 123)):
     alphabet.append(char)
 
 for char in map(chr, xrange(65, 90)):
     alphabet_upper.append(char)
+
+#Actually swap the letters
 
 for letter in input_string:
     #print letter + " -> " + str(type(letter))
